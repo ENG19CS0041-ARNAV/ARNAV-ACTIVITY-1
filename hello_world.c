891c833248e4ff78ee7ba02c67f4c8fe5dfd734c
ENG19CS0041-ARNAV <75825489+ENG19CS0041-ARNAV@users.noreply.github.com>
2021-02-03 19:53:56 +0530

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,1 +1,10 @@
 //Write the program to print "Hello World !"
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  char c[20];
+  printf("enter your name\n");
+  scanf("%s",&c);
+  printf("%s is the name",c);
+}
