d8434f4d78aa0510acc21551f7df7696dbaab8e2
ENG19CS0041-ARNAV <75825489+ENG19CS0041-ARNAV@users.noreply.github.com>
2021-02-16 15:25:35 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,23 +1,28 @@
 #include<stdio.h>
  float input(float *h,float *b,float *d)
- {
+  
+{
  printf(“Enter the value of h,b and d”);
  scanf(“%f%f%f”,h,b,d); 
 } 
-Float vol(float h,float b,float d)
- {
+
+ float vol(float h,float b,float d)
+{
  float v=((h*b*d)+(d/b))/3;
  return v;
- } 
-void print(float v1)
- {
- printf(“Volume of tromboloid is: %f”,v1);
-} 
-int main() 
+}
+
+ void print(float v1)
+{
+  printf(“Volume of tromboloid is: %f”,v1);
+}
+
+ int main() 
 { 
-float a,b,c,v1; input(&a,&b,&c);
+ float a,b,c,v1; input(&a,&b,&c);
  v1=vol(a,b,c);
+  
  print(v1); 
-return 0;
+ return 0;
 } 
 
