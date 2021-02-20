00964b9e69c0438be8d0066bae04ab52361cd3bf
ENG19CS0041-ARNAV <75825489+ENG19CS0041-ARNAV@users.noreply.github.com>
2021-02-20 18:56:30 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -6,15 +6,15 @@ int main()
 {  
     float x1, y1, x2, y2, distance;
 
-    printf(“Enter point one ”,x1,y1);
-    scanf(“%f%f”,&x1,&y1);
+    printf("Enter point one \n",x1,y1);
+    scanf("%f%f",&x1,&y1);
 
-    printf(“Enter point two”,x2,y2);
-    scanf(“%f%f”,&x2,&y2);
+    printf("Enter point two \n",x2,y2);
+    scanf("%f%f",&x2,&y2);
 
     distance= sqrt( (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) );
-
-    printf(“distance between (%f,%f) and (%f,%f) is (%f,%f)” x1,y1,x2,y2,distance);
+ 
+    printf("distance between (%0.2f,%0.2f) and (%0.2f,%0.2f) is (%0.2f,%0.2f)",x1,y1,x2,y2,distance);
      
    return 0;
 }
