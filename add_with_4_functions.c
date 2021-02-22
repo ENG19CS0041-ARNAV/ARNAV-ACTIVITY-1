4497b57863c4def71723c50e132d0348b7598390
ENG19CS0041-ARNAV <75825489+ENG19CS0041-ARNAV@users.noreply.github.com>
2021-02-22 18:55:29 +0530

    Update add_with_4_functions.c

--- add_with_4_functions.c
+++ add_with_4_functions.c
@@ -1,31 +1,30 @@
  #include<stdio.h>
- #include<conio.h>
+ int input()
 {
    int a;
-   printf(“enter a number”);
-   scanf(“%d”,a );
+   printf("enter a number\n");
+   scanf("%d",&a );
    return a;
 }
 
-int find_sum(a,b)
+int find_sum(int a,int b)
 {
    int sum;
    sum=a+b;
    return sum;
 }
 
-void output(a,b,c)
+void output(int a,int b,int c)
 {
-   printf(“sum of %d and %d is %d”,a,b,c);
+   printf("sum of %d and %d is %d\n",a,b,c);
 }
 
 int main()
 {
    int x,y,z;
    x=input();
-   y=input()
+   y=input();
    z=find_sum(x,y);
    output(x,y,z);
    return 0;
 }
-
