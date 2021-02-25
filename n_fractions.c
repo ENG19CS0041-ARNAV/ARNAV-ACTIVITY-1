bdb9d787678d3bff7a7396db7575681f9cbae99a
ENG19CS0041-ARNAV <75825489+ENG19CS0041-ARNAV@users.noreply.github.com>
2021-02-25 14:07:54 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -2,15 +2,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[20],sum;
-    printf(“enter the total numbers\n”);
-    scanf(“%d”,&n);
+    int n,a[20],sum=0;
+    printf("enter the total numbers\n");
+    scanf("%d",&n);
     for(int i=0;i<n;i++)
     {
-         printf(“enter the new number\n”);
-         scanf(“%d”,a[i]);
-         Sum = sum+a[i];
+         printf("enter the new number\n");
+         scanf("%d",&a[i]);
+         sum=sum+a[i];
     }
-    printf(“the total sum is %d\n”,sum);
+    printf("the total sum is %d\n",sum);
     return 0;
 }
