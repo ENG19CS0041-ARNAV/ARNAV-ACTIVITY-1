973115e1cba958a694074c8dbac5070146ce863c
ENG19CS0041-ARNAV <75825489+ENG19CS0041-ARNAV@users.noreply.github.com>
2021-02-25 13:56:07 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,16 @@
-//WAP to find the sum of n fractions.
+// activity 9
+#include<stdio.h>
+int main()
+{
+    int n,a[20],sum;
+    printf(“enter the total numbers\n”);
+    scanf(“%d”,&n);
+    for(int i=0;i<n;i++)
+    {
+         printf(“enter the new number\n”);
+         scanf(“%d”,a[i]);
+         Sum = sum+a[i];
+    }
+    printf(“the total sum is %d\n”,sum);
+    return 0;
+}
